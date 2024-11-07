40430bafa632b53381038b0940f896e204994082
PHARCHR <fargeta92@gmail.com>
2024-11-07 23:24:46 +0300

    10000

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(notFound);
 app.use(errorHandler);
 const start = () => {
   try {
-    connect(process.env.MONGO_URI);
+    connect(process.env.MONGO_URI||10000);
     app.listen(
       process.env.PORT,
       console.log(`Server running on port ${process.env.PORT}`)
