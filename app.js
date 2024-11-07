c5e117b15ff7d4054a280da9ab0cfee1b554d21c
PHARCHR <fargeta92@gmail.com>
2024-11-07 16:24:05 +0300

    minor editing

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const homePage = require("./middlewares/home");
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api/v1/", homePage);
+app.use(homePage);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/chatBot", authentication, botRouter);
 app.use(notFound);
